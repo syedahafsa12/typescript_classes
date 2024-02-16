fc3adac4779943bf62050d87e20ba25f8ea534fb
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2024-02-16 22:15:59 +0500

    Add files via upload

---
+++ functions/main.js
@@ -0,0 +1,13 @@
+"use strict";
+///function 
+//parametor=>>> (dahi:string)
+//argument=>>> muhammad(8)
+//named function
+// function muhammad (dahi:number){
+//     console.log('muhammad dahi le ao')
+// }
+// muhammad(8)//no error
+//function ko likhne ka dosra syntax ya format
+//arrow/lambda function
+let myfunc = (hi, hello) => console.log(hi, hello);
+console.log(myfunc(8, 9));
--- functions/main.ts
+++ functions/main.ts
@@ -1,1 +1,56 @@
+///function 
+
+//parametor=>>> (dahi:string)
+
+//argument=>>> muhammad(8)
+
+ //named function
+// function muhammad (dahi:number){
+//     console.log('muhammad dahi le ao')
+// }
+
+// muhammad(8)//no error
+
+
+//function ko likhne ka dosra syntax ya format
+
+//arrow/lambda function
+
+let myfunc = (hi:number,hello:number)=> 
+console.log(hi,hello)
+
+
+console.log(myfunc(8,9,))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
